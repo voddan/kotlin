9f2ae66883a3d20e90f75745149e2737218f4be3
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-02-20 16:10:40 +0100

    Fix for KT-16441: `NoSuchFieldError: $$delegatedProperties` when delegating through `provideDelegate` in companion object
     #KT-16441 Fixed

--- compiler/tests-ir-jvm/tests/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-ir-jvm/tests/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
@@ -6583,6 +6583,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
                 doTest(fileName);
             }
 
+            @TestMetadata("kt16441.kt")
+            public void testKt16441() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/delegatedProperty/provideDelegate/kt16441.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("local.kt")
             public void testLocal() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/delegatedProperty/provideDelegate/local.kt");
